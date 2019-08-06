dd6bb65d640c163f81fa331fb0c50c666152c720
marvin evins <maevins@cps.edu>
2019-08-06 09:54:48 -0500

    first commit

---
+++ src/collections/Collections.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList words = new ArrayList();
+		words.add("hello");
+		words.add("there");
+		
+       String item1 = (String)words.get(0);
+       String item2 = (String)words.get(1);
+	}
+
+}
